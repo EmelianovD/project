c56d4cae9dcd0a96c3345bdabc793a4eb0e2f8ee
Dmitriy.E <vinills@mail.ru>
2021-09-01 12:03:20 +0300

    comyo

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// const result= confirm('are you here?');
+//console.log(result);
+
+//const answer = prompt('вам есть 18?', '18');
+//console.log(typeof(answer));
+
+const answers = [];
+
+answers[0] = prompt('как вас зовут?', '');
+answers[1] = prompt('как вас не зовут?', '');
+answers[2] = prompt('как вас?', '');
+
+document.write(answers);
