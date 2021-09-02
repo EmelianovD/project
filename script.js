22afa83a7d7cc79abc11f0fd4f0d88cdeba14332
Dmitriy.E <vinills@mail.ru>
2021-09-02 10:59:00 +0300

    циклы while, do, for

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 "use strict";
 
-if (1) {
-  console.log('ok!'); 
-} else {
-  console.log('error!');
-}
+let num = 50;
 
-const num = 50;
+// while (num < 55) {
+//   console.log(num);
+//   num++;
+// }
 
-if (num < 49) {
-  console.log('Error!');
-} else if(num > 100) {
-  console.log('Мнонго!');
-} else  {
-  console.log('ok');
+// do {
+//   console.log(num);
+//   num++;
+// }
+// while (num < 55);
+
+for (let i = 1; i < 10; i++) {
+  if (i === 6) {
+    // break;
+    continue;
+  }
+
+  console.log(i);
 }
